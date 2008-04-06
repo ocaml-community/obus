d1777bddef7b1082c021e891378c3e3c07513153
Jeremie Dimino <jeremie@dimino.org>
2008-04-06 04:32:54 +0200

    DBus types
    darcs-hash:20080406023254-c41ad-4fba53181ba9223a7c26a490fc1303f1817f64a1

---
+++ binder/dBus.mli
@@ -0,0 +1,26 @@
+(*
+ * dBus.mli
+ * --------
+ * Copyright : (c) 2008, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, a ocaml implemtation of dbus.
+ *)
+
+type dbus_type =
+  | Tbyte
+  | Tboolean
+  | Tint16
+  | Tint32
+  | Tint64
+  | Tuint16
+  | Tuint32
+  | Tuint64
+  | Tdouble
+  | Tstring
+  | Tsignature
+  | Tobject_path
+  | Tarray of dbus_type
+  | Tdict of dbus_type * dbus_type
+  | Tstruct of dbus_type list
+  | Tvariant
