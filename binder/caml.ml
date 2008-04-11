a2bfc87df71025a0b12738d7c71b577ac58d1186
Jeremie Dimino <jeremie@dimino.org>
2008-04-12 01:10:53 +0200

    Generation of caml code for writing dbus marshaled values
    darcs-hash:20080411231053-c41ad-11262b922963508800469e2e918938dfdc7306d8

--- binder/caml.ml
+++ binder/caml.ml
@@ -15,9 +15,6 @@ type caml_patt = Ast.patt
 
 type var = string
 
-type yes
-type no
-
 type dbus_type =
   | Tbyte
   | Tboolean
@@ -36,6 +33,7 @@ type dbus_type =
   | Tstruct
   | Tvariant
   | Tcons
+  | Tnil
 
 module Gen = Generate.Make
   (struct
@@ -70,19 +68,30 @@ let t1 t x = Gen.EPLeft(t, [x])
 let t2 t x y = Gen.EPLeft(t, [x; y])
 let t3 t x y z = Gen.EPLeft(t, [x; y; z])
 
-type ('is_basic, 'is_single) dbus_reader =
+type reader =
     { reader_from : Gen.either_pattern;
       reader_vars : Gen.right_pattern list;
       reader_expr : caml_expr list -> caml_expr }
-
-type basic_reader = (yes, yes) dbus_reader
-type single_reader = (no, yes) dbus_reader
-type sequence_reader = (no, no) dbus_reader
 
 let rec split_at n l = match n, l with
   | (0, l) -> ([], l)
   | (n, x :: l) -> let b, e = split_at (n-1) l in (x :: b, e)
   | _ -> raise (Invalid_argument "split_at")
+
+let rec split_with_motif m l =
+  let rec aux m l = match m, l with
+    | [], l -> ([], l)
+    | _ :: m, e :: l -> let la, lb = aux m l in (e :: la, lb)
+    | _ -> assert false
+  in
+    match m, l with
+      | [], [] -> []
+      | tm :: hm, l -> let la, lb = aux tm l in la :: split_with_motif hm lb
+      | _ -> assert false
+
+let split2 a b l = f2 (fun x y -> (x, y)) (split_with_motif [a; b] l)
+let split3 a b c l = f3 (fun x y z -> (x, y, z)) (split_with_motif [a; b; c] l)
+
 
 let rec signature_of_term = function
   | Gen.EPLeft(t, args) -> begin match t, args with
@@ -107,7 +116,7 @@ let rec signature_of_term = function
     end
   | _ -> raise (Invalid_argument "signature_of_term")
 
-let right_from_caml_type t =
+let right_pattern_from_caml_type t =
   let rec aux = function
     | (<:ctyp< $id:t$ >>) -> ([], t)
     | (<:ctyp< $a$ $b$ >>) -> let args, id = aux b in
@@ -196,7 +205,7 @@ let rdict key_reader val_reader f x =
   { reader_from = t2 Tdict key_reader.reader_from val_reader.reader_from;
     reader_vars = key_reader.reader_vars @ val_reader.reader_vars;
     reader_expr = (fun l ->
-                     let key_args, val_args = split_at (List.length key_reader.reader_vars) l in
+                     let key_args, val_args = split2 key_reader.reader_vars val_reader.reader_vars l in
                        <:expr< read_dict
                          $key_reader.reader_expr key_args$
                          $val_reader.reader_expr val_args$
@@ -225,68 +234,18 @@ let rfixed_variant chooser branches =
                                            $reader.reader_expr []$ >>),
                                expr)
                     end (Ast.McNil _loc) branches)) }
-
-(*                    List.fold_right begin fun
-
-<:expr< match <:expr< chooser >> with
-          [ $List.fold_left begin fun (patt, reader) ->
-              Ast.RbSem(_loc, $patt$,
-                        <:expr< read_fixed_variant
-                          $signature_of_term reader.reader_from$
-                          $reader.reader_expr []$ >>)
-
-List.map begin fun (patt, reader) ->
-                 <:patt< $patt$ -> read_fixed_variant
-                                 $signature_of_term reader.reader_from$
-                                 $reader.reader_expr []$ >>
-               end branches$ |] ] >> }
-
-let print_bitfield_from_char name items =
-  let bit i = string_of_int (1 lsl i) in
-  let aux_item expr (item,i) =
-    let item = camlize_field item in
-    let assign = <:expr< bit_value land $int:bit i$ <> 0 >> in
-      Ast.RbSem (_loc,
-                 Ast.RbEq (_loc,
-                           Ast.IdLid (_loc, item),
-                           assign
-                          ),
-                 expr
-                )
-  in
-  let make_bitfield = Ast.ExRec(_loc, List.fold_left aux_item (Ast.RbNil _loc) items, Ast.ExNil _loc) in
-  let fun_name = from_char_name name in
-    <:str_item< let $lid:fun_name$ char =
-      let bit_value = int_of_char char in
-        $make_bitfield$
-        >>*)
-
-(*let rnil =
-  { reader_from = t0 Tnil;
-    reader_vars = [];
-    reader_expr = f0 *)
-let rcons a v b = (*match b.reader_from with
-  | EPLeft(Tcons, _) ->*)
-      { reader_from = t2 Tcons a.reader_from b.reader_from;
-        reader_vars = a.reader_vars @ b.reader_vars;
-        reader_expr = begin fun l ->
-          let la, lb = split_at (List.length a.reader_vars) l in
-            <:expr< fun i ->
-              let i, $v$ = $a.reader_expr la$ i in
-                $b.reader_expr lb$ i >>
-        end }
-(*  | _ ->
-      { reader_from = t2 Tcons a.reader_from (t2 Tcons b.reader_from (Gen.EPVar "__tail"));
-        reader_vars = Gen.EPVar "__tail" :: (a.reader_vars @ b.reader_vars);
-        reader_expr = begin function
-          | tail :: l ->
-              let la, lb = split_at (List.length a.reader_vars) l in
-                (<:expr< fun i ->
-                   let i, $v$ = $a la$ i in
-                   let i, vh = $b lb$ i in
-                   let i, vt = $tail$ in
-                     (vh, vt) >>)
-          | _ -> assert false }*)
+let rseq readers result =
+  List.fold_right begin fun (patt, reader) acc ->
+    { reader_from = t2 Tcons reader.reader_from acc.reader_from;
+      reader_vars = reader.reader_vars @ acc.reader_vars;
+      reader_expr = fun l ->
+        let la, lb = split2 reader.reader_vars acc.reader_vars l in
+          <:expr<
+            let i, $patt$ = $reader.reader_expr la$ i in
+              $acc.reader_expr lb$ i >> }
+  end readers { reader_from = t0 Tnil;
+                reader_vars = [];
+                reader_expr = f0 result }
 let rbind r v e =
   { reader_from = r.reader_from;
     reader_vars = r.reader_vars;
@@ -296,26 +255,27 @@ let rbind r v e =
           i, $e$ >>
     end }
 let rcaml t =
-  { reader_from = Gen.EPRight(right_from_caml_type t);
-    reader_vars = [right_from_caml_type t];
-    reader_expr = f1 (fun x -> x) }
+  let rp = right_pattern_from_caml_type t in
+    { reader_from = Gen.EPRight(rp);
+      reader_vars = [rp];
+      reader_expr = f1 (fun x -> x) }
 let rv x =
   { reader_from = Gen.EPVar(x);
     reader_vars = [Gen.RPVar(x)];
     reader_expr = f1 (fun x -> x) }
 
-type reader = Gen.generator
+type reading_rule = Gen.generator
 
-let reader reader caml_type =
+let make_reading_rule reader caml_type =
   Gen.make_generator
     reader.reader_from
-    (right_from_caml_type caml_type)
+    (right_pattern_from_caml_type caml_type)
     reader.reader_vars
     reader.reader_expr
 
-let (-->) a b = reader a b
+let (-->) a b = make_reading_rule a b
 
-let default_readers = [
+let default_reading_rules = [
   rbyte --> <:ctyp< char >>;
   rboolean --> <:ctyp< bool >>;
   rint16 --> <:ctyp< int >>;
@@ -332,7 +292,7 @@ let default_readers = [
   rstring --> <:ctyp< string >>;
   rsignature_as_string --> <:ctyp< string >>;
   rsignature --> <:ctyp< OBus.dbus_type >>;
-  robject_path --> <:ctyp< into_string >>;
+  robject_path --> <:ctyp< string >>;
   rarray (rv"a") <:expr< (::) >> <:expr< [] >> --> <:ctyp< 'a list >>;
   rdict (rv"k") (rv"v") <:expr< fun k v l -> (k, v) :: l >> <:expr< [] >> --> <:ctyp< ('k, 'v) list >>;
   rvariant --> <:ctyp< OBus.dbus_value >>;
@@ -354,7 +314,11 @@ let rec left_from_dbus_type = function
   | DBus.Tobject_path -> Gen.Term(Tobject_path, [])
   | DBus.Tarray(t) -> Gen.Term(Tarray, [left_from_dbus_type t])
   | DBus.Tdict(tk, tv) -> Gen.Term(Tdict, [left_from_dbus_type tk; left_from_dbus_type tv])
-  | DBus.Tstruct(tl) -> Gen.Term(Tstruct, [])
+  | DBus.Tstruct(tl) ->
+      Gen.Term(Tstruct,
+               [List.fold_right (fun t acc ->
+                                   Gen.Term(Tcons, [left_from_dbus_type t; acc]))
+                  tl (Gen.Term(Tnil, []))])
 (*                             match tl with
                                | [] -> []
                                | x::l -> [List.fold_left (fun l x -> Gen.Term(Tcons,  left_from_dbus_type t])*)
@@ -372,5 +336,194 @@ let right_from_caml_type t =
   in
     aux2 t
 
-let generate readers dbust camlt =
-  Gen.generate readers (left_from_dbus_type dbust) (right_from_caml_type camlt)
+let generate_reader rules dbust camlt =
+  Gen.generate rules (left_from_dbus_type dbust) (right_from_caml_type camlt)
+
+type writer =
+    { writer_to : Gen.either_pattern;
+      writer_vars : Gen.right_pattern list;
+      writer_expr : caml_expr list -> caml_expr }
+
+let wint16 =
+  { writer_to = t0 Tint16;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_int16 >> }
+let wint32 =
+  { writer_to = t0 Tint32;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_int32 >> }
+let wint64 =
+  { writer_to = t0 Tint64;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_int64 >> }
+let wuint16=
+  { writer_to = t0 Tuint16;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_uint16 >> }
+let wuint32=
+  { writer_to = t0 Tuint32;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_uint32 >> }
+let wuint64=
+  { writer_to = t0 Tuint64;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_uint64 >> }
+let wint32_from_int32 =
+  { writer_to = t0 Tint32;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_int32_from_int32 >> }
+let wint64_from_int64 =
+  { writer_to = t0 Tint64;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_int64_from_int64 >> }
+let wuint32_from_int32 =
+  { writer_to = t0 Tuint32;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_uint32_from_int32 >> }
+let wuint64_from_int64 =
+  { writer_to = t0 Tuint64;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_uint64_from_int64 >> }
+let wsignature =
+  { writer_to = t0 Tsignature;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_signature >> }
+let wsignature_from_string =
+  { writer_to = t0 Tsignature;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_signature_from_string >> }
+let wbyte =
+  { writer_to = t0 Tbyte;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_byte >> }
+let wboolean =
+  { writer_to = t0 Tboolean;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_boolean >> }
+let wdouble =
+  { writer_to = t0 Tdouble;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_double >> }
+let wstring =
+  { writer_to = t0 Tstring;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_string >> }
+let wobject_path =
+  { writer_to = t0 Tobject_path;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_object_path >> }
+let warray elt_writer fold =
+  { writer_to = t1 Tarray elt_writer.writer_to;
+    writer_vars = elt_writer.writer_vars;
+    writer_expr = fun l ->
+      <:expr< write_array
+        $elt_writer.writer_expr l$
+        $fold$ >> }
+let wdict key_writer val_writer fold =
+  { writer_to = t2 Tdict key_writer.writer_to val_writer.writer_to;
+    writer_vars = key_writer.writer_vars @ val_writer.writer_vars;
+    writer_expr = fun l ->
+      let lk, lv = split2 key_writer.writer_vars val_writer.writer_vars l in
+        <:expr< write_dict
+          $key_writer.writer_expr lk$
+          $val_writer.writer_expr lv$
+          $fold$ >> }
+let wstruct writer =
+  { writer_to = t1 Tstruct writer.writer_to;
+    writer_vars = writer.writer_vars;
+    writer_expr = fun l ->
+      <:expr< write_struct $writer.writer_expr l$ >> }
+let wvariant =
+  { writer_to = t0 Tvariant;
+    writer_vars = [];
+    writer_expr = f0 <:expr< write_variant >> }
+let wfixed_variant branches common_writer =
+  { writer_to = t0 Tvariant;
+    writer_vars = common_writer.writer_vars;
+    writer_expr = fun l ->
+      <:expr< fun x i ->
+        $Ast.ExMat(_loc,
+                   <:expr< x >>,
+                   List.fold_left begin fun acc (patt, common_expr, expr, writer) ->
+                     Ast.McOr(_loc,
+                              Ast.McArr(_loc,
+                                        patt,
+                                        Ast.ExNil _loc,
+                                        <:expr<
+                                          let i = $common_writer.writer_expr l$ $common_expr$ i in
+                                            $writer.writer_expr []$ $expr$ i
+                                            >>),
+                              acc)
+                   end (Ast.McNil _loc) branches)$ >> }
+let wcons a b =
+  { writer_to = t2 Tcons a.writer_to b.writer_to;
+    writer_vars = a.writer_vars @ b.writer_vars;
+    writer_expr = fun l ->
+      let la, lb = split2 a.writer_vars b.writer_vars l in
+        <:expr< fun (x,y) i ->
+          let i = $a.writer_expr la$ x i in
+            $b.writer_expr lb$ x i >> }
+let wcaml t =
+  let rp = right_pattern_from_caml_type t in
+    { writer_to = Gen.EPRight(rp);
+      writer_vars = [rp];
+      writer_expr = f1 (fun x -> x) }
+let wv v =
+  { writer_to = Gen.EPVar v;
+    writer_vars = [Gen.RPVar v];
+    writer_expr = f1 (fun x -> x) }
+let wconv expr writer =
+  { writer_to = writer.writer_to;
+    writer_vars = writer.writer_vars;
+    writer_expr = fun l ->
+      <:expr< fun x -> $writer.writer_expr l$ ($expr$ x) >> }
+
+type writing_rule = Gen.generator
+
+let make_writing_rule writer caml_type =
+  Gen.make_generator
+    writer.writer_to
+    (right_pattern_from_caml_type caml_type)
+    writer.writer_vars
+    writer.writer_expr
+
+let generate_writer rules camlt dbust =
+  Gen.generate rules (left_from_dbus_type dbust) (right_from_caml_type camlt)
+
+let (<--) a b = make_writing_rule a b
+
+let default_writing_rules = [
+  wbyte <-- <:ctyp< char >>;
+  wboolean <-- <:ctyp< bool >>;
+  wint16 <-- <:ctyp< int >>;
+  wint32 <-- <:ctyp< int >>;
+  wint64 <-- <:ctyp< int >>;
+  wint32_from_int32 <-- <:ctyp< int32 >>;
+  wint64_from_int64 <-- <:ctyp< int64 >>;
+  wuint16 <-- <:ctyp< int >>;
+  wuint32 <-- <:ctyp< int >>;
+  wuint64 <-- <:ctyp< int >>;
+  wuint32_from_int32 <-- <:ctyp< int32 >>;
+  wuint64_from_int64 <-- <:ctyp< int64 >>;
+  wdouble <-- <:ctyp< float >>;
+  wstring <-- <:ctyp< string >>;
+  wsignature_from_string <-- <:ctyp< string >>;
+  wsignature <-- <:ctyp< OBus.dbus_type >>;
+  wobject_path <-- <:ctyp< string >>;
+  warray (wv"a") (<:expr< fun f l x -> List.fold_left f x l >>) <-- <:ctyp< 'a list >>;
+  wdict (wv"k") (wv"v") (<:expr< fun f l x -> List.fold_left (fun acc (k, v) -> f k v acc) f l x >>) <-- <:ctyp< ('k, 'v) list >>;
+  wvariant <-- <:ctyp< OBus.dbus_value >>;
+  wconv <:expr< char_of_int >> (wcaml <:ctyp< char >>) <-- <:ctyp< int >>
+]
+
+
+let map_reading mod_name key_type =
+  let mod_expr = <:ident< $uid:String.capitalize mod_name$ >> in
+    rdict (rcaml key_type) (rv"a")
+      (<:expr< $id:mod_expr$ . add >>) (<:expr< $id:mod_expr$ . empty >>)
+    --> Ast.TyId(_loc, Ast.IdAcc(_loc, mod_expr, Ast.IdLid(_loc, "i")))
+
+let map_writing mod_name key_type =
+  let mod_expr = <:ident< $uid:String.capitalize mod_name$ >> in
+    wdict (wcaml key_type) (wv"a")
+      (<:expr< $id:mod_expr$ . fold >>) <-- Ast.TyId(_loc, Ast.IdAcc(_loc, mod_expr, Ast.IdLid(_loc, "i")))
--- binder/caml.mli
+++ binder/caml.mli
@@ -13,68 +13,64 @@ type caml_type = Camlp4.PreCast.Ast.ctyp
 
 type var = string
 
-type yes
-type no
+(** {6 Reading dbus marshaled value into caml value} *)
 
-type ('is_basic, 'is_single) dbus_reader
-type basic_reader = (yes, yes) dbus_reader
-type single_reader = (no, yes) dbus_reader
-type sequence_reader = (no, no) dbus_reader
-    (** Types for dbus values *)
+type reader
+  (** Type of a dbus reader *)
 
-val rint16 : basic_reader
-val rint32 : basic_reader
-val rint64 : basic_reader
-val ruint16 : basic_reader
-val ruint32 : basic_reader
-val ruint64 : basic_reader
-  (** Reader for the various dbus int types, this readers always
+val rint16 : reader
+val rint32 : reader
+val rint64 : reader
+val ruint16 : reader
+val ruint32 : reader
+val ruint64 : reader
+  (** Reader for the various dbus int types, these readers always
       return a caml int *)
 
-val rint32_as_int32 : basic_reader
-val rint64_as_int64 : basic_reader
-val ruint32_as_int32 : basic_reader
-val ruint64_as_int64 : basic_reader
+val rint32_as_int32 : reader
+val rint64_as_int64 : reader
+val ruint32_as_int32 : reader
+val ruint64_as_int64 : reader
   (** Same thing but return int32 and int64 caml value *)
 
-val rsignature : basic_reader
+val rsignature : reader
   (** Read a dbus signature into a OBus.dbus_type *)
 
-val rsignature_as_string : basic_reader
+val rsignature_as_string : reader
   (** Read a signature into a string representing the dbus type using
       dbus type codes *)
 
-val rbyte : basic_reader
+val rbyte : reader
   (** Read a dbus byte into a caml char *)
 
-val rboolean : basic_reader
+val rboolean : reader
   (** Read a dbus boolean into a caml bool *)
 
-val rdouble : basic_reader
-  (** Read a dbus double into a caml double *)
+val rdouble : reader
+  (** Read a dbus double into a caml float *)
 
-val rstring : basic_reader
+val rstring : reader
   (** Read a dbus string into a caml string *)
 
-val robject_path : basic_reader
-  (** Other dbus basic value readers *)
+val robject_path : reader
+  (** Read a dbus object path into a caml string *)
 
-val rarray : (_, yes) dbus_reader -> caml_expr -> caml_expr -> single_reader
+val rarray : reader -> caml_expr -> caml_expr -> reader
   (** [rarray reader f x] correspond to a function that read a dbus
       array [|a1; a2; ...; an|] using [reader] and construct the caml
       value (f an (... (f a2 (f a1 x)) ...)) *)
 
-val rdict : (yes, yes) dbus_reader -> (_, yes) dbus_reader -> caml_expr -> caml_expr -> single_reader
-  (** Same thing as darray but for dbus dictionary *)
+val rdict : reader -> reader -> caml_expr -> caml_expr -> reader
+  (** Same thing as rarray but for dbus dictionary *)
 
-val rstruct : (_, _) dbus_reader -> single_reader
-  (** [dstruct seq] read a sequence of dbus value packed into a dbus
+val rstruct : reader -> reader
+  (** [rstruct seq] read a sequence of dbus value packed into a dbus
       struct *)
 
-val rvariant : single_reader
+val rvariant : reader
   (** [rvariant] read a dbus variant into a caml OBus.dbus_value *)
 
-val rfixed_variant : caml_expr -> (caml_patt * (_, yes) dbus_reader) list -> single_reader
+val rfixed_variant : caml_expr -> (caml_patt * reader) list -> reader
   (** [rfixed_variant e branches] read a variant into a caml value
       according to the value of [e].
 
@@ -83,10 +79,10 @@ val rfixed_variant : caml_expr -> (caml_patt * (_, yes) dbus_reader) list -> sin
       content of the variant depend on the value of the first byte, so
       we can use this reader :
 
-      dcons dbyte <:patt< b >> (dfixed_variant <:expr< b >>
-        [ <:patt< 0 >> --> dbind dstring "x" <:expr< Sender(x) >>;
-          <:patt< 1 >> --> dbind duint32 "x" <:expr< Serial(x) >>;
-          ... ]
+      rseq [rbyte, <:patt< b >>; rfixed_variant <:expr< b >>
+              [ <:patt< 0 >>, rbind rstring "x" <:expr< Sender(x) >>;
+                <:patt< 1 >>, rbind ruint32 "x" <:expr< Serial(x) >>;
+                ... ], <:patt< r >>] <:expr< r >>
 
       Note: as the content type of the variant is not known, readers
       have to be "concrete readers", so they must not contains any
@@ -94,15 +90,137 @@ val rfixed_variant : caml_expr -> (caml_patt * (_, yes) dbus_reader) list -> sin
       case.
   *)
 
-val rcons : (_, yes) dbus_reader -> caml_patt -> (_, _) dbus_reader -> sequence_reader
-  (** [rcons head var tail] read a single dbus value with [head], then
-      bind its result to [var] and read others value with [tail] *)
+val rseq : (caml_patt * reader) list -> caml_expr -> reader
+  (** [rseq readers expr] read a sequence of value using [readers],
+      then evalutate [expr] *)
 
-val rbind : ('a, 'b) dbus_reader -> caml_patt -> caml_expr -> ('a, 'b) dbus_reader
+val rbind : reader -> caml_patt -> caml_expr -> reader
   (** [rbind reader var expr] read a dbus value with [reader], bind
       its result to [var] in [expr].
 
       For example if you want to convert an dbus uint32 into a caml
+      type:
+
+      type t = A | B
+
+      just write something like that:
+
+      rbind ruint32 <:patt< x >> <:expr< match x with
+        | 0 -> A
+        | 1 -> B >>
+  *)
+
+val rcaml : caml_type -> reader
+  (** [rcaml caml_type] read a dbus value into a caml value of type
+      [caml_type] if we already know how to read it *)
+
+val rv : string -> reader
+  (** [rv var] read any dbus value that we know how to read *)
+
+(** {6 Reading rules} *)
+
+type reading_rule
+
+val make_reading_rule : reader -> caml_type -> reading_rule
+  (** [make_reading_rule dbus_reader caml_type] create a rule for
+      reading dbus value into caml value of type [caml_type] using
+      [dbus_reader]. The set of variables that appears in
+      [dbus_reader] must be the same as the one who appears in
+      [caml_type], in other case an [Invalid_argument] is raised *)
+
+val default_reading_rules : reading_rule list
+  (** Default reading rules *)
+
+(** {6 Writing caml value as dbus marshaled value} *)
+
+type writer
+  (** Type of a dbus writer *)
+
+val wint16 : writer
+val wint32 : writer
+val wint64 : writer
+val wuint16 : writer
+val wuint32 : writer
+val wuint64 : writer
+  (** Writer for the various dbus int types, these writers always take
+      a caml int *)
+
+val wint32_from_int32 : writer
+val wint64_from_int64 : writer
+val wuint32_from_int32 : writer
+val wuint64_from_int64 : writer
+  (** Same thing but take int32 and int64 caml value *)
+
+val wsignature : writer
+  (** Write a dbus signature from a OBus.dbus_type *)
+
+val wsignature_from_string : writer
+  (** Write a signature from a string representing the dbus type using
+      dbus type codes *)
+
+val wbyte : writer
+  (** Write a dbus byte from a caml char *)
+
+val wboolean : writer
+  (** Write a dbus boolean from a caml bool *)
+
+val wdouble : writer
+  (** Write a dbus double from a caml float *)
+
+val wstring : writer
+  (** Write a dbus string from a caml string *)
+
+val wobject_path : writer
+  (** Write a dbus object path from a caml string *)
+
+
+val warray : writer -> caml_expr -> writer
+  (** [warray expr writer fold] correspond to a function that write a dbus
+      array using [fold] and [writer]. [fold] is act as
+      List.fold_left *)
+
+val wdict : writer -> writer -> caml_expr -> writer
+  (** Same thing as warray but for dbus dictionary *)
+
+val wstruct : writer -> writer
+  (** [wstruct seq] write a sequence of dbus value *)
+
+val wvariant : writer
+  (** [wvariant] write a dbus variant from a caml OBus.dbus_value *)
+
+val wfixed_variant : (caml_patt * caml_expr * caml_expr * writer) list -> writer -> writer
+  (** [wfixed_variant e branches] read a variant from a caml value
+      according to the value of [e].
+
+      For example in a dbus message header, the additionnal header
+      fields are represented as byte * variant dbus value and the
+      content of the variant depend on the value of the first byte, so
+      we can use this writer :
+
+      dcons dbyte <:patt< b >> (dfixed_variant <:expr< b >>
+        [ <:patt< 0 >>, dbind dstring "x" <:expr< Sender(x) >>;
+          <:patt< 1 >>, dbind duint32 "x" <:expr< Serial(x) >>;
+          ... ]
+
+      Note: as the content type of the variant is not known, writers
+      have to be "concrete writers", so they must not contains any
+      variable or rcaml. An invalid argument is raised in other
+      case.
+  *)
+
+val wcons : writer -> writer -> writer
+  (** [wcons head var tail] read a single dbus value with [head], then
+      bind its result to [var] and read others value with [tail] *)
+
+(*val wnil : caml_expr -> sequence reader*)
+  (** [wnil expr] Write the end of a sequence of value, for example
+      the end of a structure *)
+
+(*val wbind : ('a, 'b) dbus writer -> caml_patt -> caml_expr -> ('a, 'b) dbus writer*)
+  (** [wbind writer var expr] read a dbus value with [writer], bind
+      its result to [var] in [expr].
+
+      For example if you want to convert an dbus uint32 from a caml
       type:
 
       type t = A | B
@@ -114,28 +232,49 @@ val rbind : ('a, 'b) dbus_reader -> caml_patt -> caml_expr -> ('a, 'b) dbus_read
                                            | 1 -> B >>
   *)
 
-val rcaml : caml_type -> (_, _) dbus_reader
-  (** [rcaml caml_type] read a dbus value into a caml value of type
+val wconv : caml_expr -> writer -> writer
+  (** [wconv expr writer] use [expr] to convert the value to write,
+      then use [writer] to write it *)
+
+val wcaml : caml_type -> writer
+  (** [wcaml caml_type] read a dbus value from a caml value of type
       [caml_type] if we already know how to read it *)
 
-val rv : string -> (_, _) dbus_reader
-  (** [rv var] read any dbus value that we know how to read *)
+val wv : string -> writer
+  (** [wv var] read any dbus value that we know how to read *)
 
-(** {6 Reading rules} *)
+(** {6 Writing rules} *)
 
-type reader
+type writing_rule
 
-val reader : ('a, 'b) dbus_reader -> caml_type -> reader
-  (** [reader dbus_reader caml_type] create a rule for reading dbus
-      value into caml value of type [caml_type] using [dbus_reader].
-      The set of variables that appears in [dbus_reader] must be the
-      same as the one who appears in [caml_type], in other case an
-      [Invalid_argument] is raised *)
+val make_writing_rule : writer -> caml_type -> writing_rule
+  (** [make_writing_rule dbus_writer caml_type] create a rule for
+      writing dbus value from caml value of type [caml_type] using
+      [dbus_writer]. The set of variables that appears in
+      [dbus_writer] must be the same as the one who appears in
+      [caml_type], in other case an [Invalid_argument] is raised *)
 
-val default_readers : reader list
-  (** Default reader rules *)
+val default_writing_rules : writing_rule list
+  (** Default writing rules *)
+
+(** {6 Helpers} *)
+
+val map_reading : string -> caml_type -> reading_rule
+  (** [map_reading module_name key_type] create a rule for reading a map
+      created with Map.Make. [module_name] is the name of the module,
+      it is automatically capitalized. *)
+
+val map_writing : string -> caml_type -> reading_rule
+  (** Same as [map_reading] but for writing. *)
 
 (** {6 Code generation} *)
 
-val generate : reader list -> DBus.dbus_type -> caml_type -> caml_expr option
+val generate_reader : reading_rule list -> DBus.dbus_type -> caml_type -> caml_expr option
+  (** [generate_reader rules dbus_type caml_type] generate a caml
+      expression that read a dbus marshaled value of type [dbus_type]
+      from a caml value of type [caml_type] *)
 
+val generate_writer : writing_rule list -> caml_type -> DBus.dbus_type -> caml_expr option
+  (** [generate_writer rules caml_type dbus_type] generate a caml
+      expression that write a dbus marshaled value of type [dbus_type]
+      from a caml value of type [caml_type] *)
