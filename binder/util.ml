7fef2806613ca9fc3bfd6266063446a1a2b27bb1
Jeremie Dimino <jeremie@dimino.org>
2008-04-17 07:02:59 +0200

    utils
    darcs-hash:20080417050259-c41ad-ce6b01ec7d05e984d3e50fc0d9d8358200882d4d

---
+++ binder/util.ml
@@ -0,0 +1,24 @@
+(*
+ * util.ml
+ * -------
+ * Copyright : (c) 2008, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, a ocaml implemtation of dbus.
+ *)
+
+let try_finally f close arg =
+  let result =
+    try
+      f arg
+    with
+      | e -> close arg; raise e
+  in
+    close arg;
+    result
+
+let with_open_in fname f =
+  try_finally f close_in (open_in fname)
+
+let with_open_out fname f =
+  try_finally f close_out (open_out fname)
---
+++ binder/util.mli
@@ -0,0 +1,11 @@
+(*
+ * util.mli
+ * --------
+ * Copyright : (c) 2008, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, a ocaml implemtation of dbus.
+ *)
+
+val with_open_in : string -> (in_channel -> 'a) -> 'a
+val with_open_out : string -> (out_channel -> 'a) -> 'a
