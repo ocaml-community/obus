36846c6a2ade0b8c6c62263b63c407c6285a2136
Jeremie Dimino <jeremie@dimino.org>
2008-04-18 22:36:23 +0200

    typo
    darcs-hash:20080418203623-c41ad-ac92e0a80dd9c8512838d350fb36884d25bb9689

--- binder/binder.ml
+++ binder/binder.ml
@@ -4,7 +4,7 @@
  * Copyright : (c) 2008, Jeremie Dimino <jeremie@dimino.org>
  * Licence   : BSD3
  *
- * This file is a part of obus, a ocaml implemtation of dbus.
+ * This file is a part of obus, an ocaml implemtation of dbus.
  *)
 
 module MakeMap(L : Language.S) =
