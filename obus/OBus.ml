29ceb38ca401480202c92d3df104fec8028d648c
Jeremie Dimino <jeremie@dimino.org>
2008-04-21 03:49:01 +0200

    cleaner interface
    darcs-hash:20080421014901-c41ad-09a37b3db6f0f72569cea0990f07b8a9b5554828

--- obus/OBus.ml
+++ obus/OBus.ml
@@ -8,18 +8,24 @@
  *)
 
 module LowLevel = LowLevel_internal
+
+module Bus =
+struct
+  type t = LowLevel.bus
+  exception Connection_failed = LowLevel.Connection_failed
+  let session () = LowLevel.open_bus (Address.session ())
+  let system () = LowLevel.open_bus (Address.system ())
+  let connect str = LowLevel.open_bus (Address.of_string str)
+end
+
 module Types = Types_internal
 module Values = Values_internal
 
-type bus = LowLevel.bus
-
-exception Connection_failed
-
-let session () =
-  LowLevel.open_bus (Address.session ())
-
-let system () =
-  LowLevel.open_bus (Address.system ())
-
-let connect_to_bus str =
-  LowLevel.open_bus (Address.of_string str)
+module Signal =
+struct
+  type 'a t = int
+  type id = int
+  let register bus signal f = failwith "not implemented"
+  let unregister bus id = failwith "not implemented"
+  let clear bus signal = failwith "not implemented"
+end
--- obus/OBus.mli
+++ obus/OBus.mli
@@ -7,30 +7,66 @@
  * This file is a part of obus, an ocaml implemtation of dbus.
  *)
 
+(** OBus main module.
+
+    Note: this module is destined to be used with automatically
+    generated dbus interfaces.
+
+    You can not send arbitrary data over the bus, if this is what you
+    want, look at the lowlevel library.
+*)
+
 (** {6 Connection} *)
 
-type bus
-  (** Abstract type for a message bus *)
+module Bus : sig
+  type t
 
-exception Connection_failed
+  exception Connection_failed of string
 
-val session : unit -> bus
-  (** [session ()] establish a connection with the session message bus *)
+  val session : unit -> t
+    (** [session ()] establish a connection with the session message bus *)
 
-val system : unit -> bus
-  (** [connect ()] establish a connection with the system message bus *)
+  val system : unit -> t
+    (** [connect ()] establish a connection with the system message bus *)
 
-val connect_to_bus : string -> bus
-  (** [connect_to_bus address] establish a connection with the bus
-      which address is described by [address] *)
+  val connect : string -> t
+    (** [connect address] establish a connection with the bus which
+        address is described by [address] *)
+
+  val dispatch : bus -> unit
+    (** [dispatch bus] read all pending call *)
+
+  val fd : t -> Unix.file_descr
+    (** [fd bus] get the file descriptor associated with the bus (for
+        select) *)
+end
 
 (** {6 DBus values and types} *)
 
 module Types : Types.S
+  (** Representation of dbus types *)
+
 module Values : Values.S
+  (** Representation of dbus values *)
+  with module T = Types
 
-(** {6 Low level operations} *)
+(** {6 Signals} *)
 
-module LowLevel : LowLevel.S with type bus = bus
-                             and module T = Types
-                             and module V = Values
+module Signal : sig
+  type 'a t
+
+  type id
+    (** Identifier for a signal handler *)
+
+  val register : Bus.t -> 'a t -> ('a -> unit) -> id
+    (** [register bus signal handler] add a signal handler. Each time
+        the signal [signal] came from the bus [handler] will be called
+        with the signal arguments as parameters. *)
+
+  val unregister : Bus.t -> id -> unit
+    (** [unregister bus id] delete a handler *)
+
+  val clear : Bus.t -> 'a t -> unit
+    (** [clear bus signal] clear all signal handler for the given
+        signal *)
+end
