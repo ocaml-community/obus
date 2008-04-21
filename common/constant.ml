c030fcd06b0a0830a88fd3e7fd20f9f407f619d9
Jeremie Dimino <jeremie@dimino.org>
2008-04-21 06:22:05 +0200

    constants
    darcs-hash:20080421042205-c41ad-6c289b97ece03507704894ced1193b0d33660f5c

---
+++ common/constant.ml
@@ -0,0 +1,11 @@
+(*
+ * constant.ml
+ * -----------
+ * Copyright : (c) 2008, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, an ocaml implemtation of dbus.
+ *)
+
+let max_array_size = 1 lsl 26
+let max_message_size = 1 lsl 27
---
+++ common/constant.mli
@@ -0,0 +1,14 @@
+(*
+ * constant.mli
+ * ------------
+ * Copyright : (c) 2008, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, an ocaml implemtation of dbus.
+ *)
+
+val max_array_size : int
+  (** Maximum size of an array *)
+
+val max_message_size : int
+  (** Maximum size of a message *)
