4d33ab8c7278750fb4b1e84702473a5aa7075e74
Jeremie Dimino <jeremie@dimino.org>
2008-05-29 17:50:19 +0200

    make the compilation with C stubs to work + a small benchmark
    darcs-hash:20080529155019-c41ad-521e91064239206dce9cc87fe22d644c3a13d6d3

--- compile-samples.sh
+++ compile-samples.sh
@@ -3,9 +3,9 @@ LOWLEVELS="hello hello2 notify"
 PROXIES="eject bus-functions"
 samples=""
 for i in $LOWLEVELS; do
-    samples+=" samples/lowlevels/$i.byte"
+    samples="$samples samples/lowlevels/$i.byte"
 done
 for i in $PROXIES; do
-    samples+=" samples/proxies/$i.byte"
+    samples="$samples samples/proxies/$i.byte"
 done
 exec ocamlbuild $samples
