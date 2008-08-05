07230fa24427ac176b12cb743d9af8e9c04813c3
Jeremie Dimino <jeremie@dimino.org>
2008-08-06 00:03:15 +0200

    simplification of annotations
    with too many convertion it is becoming confising and generating
    annotations is not very usefull.
    
    darcs-hash:20080805220315-c41ad-618deded7e6875fcabea89135b59a4e4e40ad1cf

--- obus/oBus_types.ml
+++ obus/oBus_types.ml
@@ -88,3 +88,73 @@ let signature_of_string signature =
       Parse_failure(i, msg) ->
         raise (Invalid_argument
                  (sprintf "signature_of_string: invalid signature %S, at position %d: %s" signature i msg))
+
+type (+'a, +'b) annot = Annot of (sequence -> sequence)
+type (+'a, +'b) one = ('a, 'b * 'a) annot
+
+type dbasic_byte
+type dbasic_boolean
+type dbasic_int16
+type dbasic_int32
+type dbasic_int64
+type dbasic_uint16
+type dbasic_uint32
+type dbasic_uint64
+type dbasic_double
+type dbasic_string
+type dbasic_signature
+type dbasic_object_path
+type 'a dbasic
+type dbyte = dbasic_byte dbasic
+type dboolean = dbasic_boolean dbasic
+type dint16 = dbasic_int16 dbasic
+type dint32 = dbasic_int32 dbasic
+type dint64 = dbasic_int64 dbasic
+type duint16 = dbasic_uint16 dbasic
+type duint32 = dbasic_uint32 dbasic
+type duint64 = dbasic_uint64 dbasic
+type ddouble = dbasic_double dbasic
+type dstring = dbasic_string dbasic
+type dsignature = dbasic_signature dbasic
+type dobject_path = dbasic_object_path dbasic
+type 'a dstruct
+type 'a darray
+type ('a, 'b) ddict
+constraint 'a = _ dbasic
+type dvariant
+type 'a basic_p = (unit, 'a) one
+constraint 'a = _ dbasic
+type 'a single_p = (unit, 'a) one
+type 'a sequence_p = (unit, 'a) annot
+
+let basic_of_annot (Annot seq) = match seq [] with
+  | [Tbasic t] -> t
+  | _ -> assert false
+      (* Phantom types assure that this will never happen *)
+let single_of_annot (Annot seq) = match seq [] with
+  | [t] -> t
+  | _ -> assert false
+let sequence_of_annot (Annot seq) = seq []
+
+let one t = fun l -> t :: l
+let one_basic t = one (Tbasic t)
+
+let dbyte = Annot(one_basic Tbyte)
+let dboolean = Annot(one_basic Tboolean)
+let dint16 = Annot(one_basic Tint16)
+let dint32 = Annot(one_basic Tint32)
+let dint64 = Annot(one_basic Tint64)
+let duint16 = Annot(one_basic Tuint16)
+let duint32 = Annot(one_basic Tuint32)
+let duint64 = Annot(one_basic Tuint64)
+let ddouble = Annot(one_basic Tdouble)
+let dstring = Annot(one_basic Tstring)
+let dsignature = Annot(one_basic Tsignature)
+let dobject_path = Annot(one_basic Tobject_path)
+let dstruct (Annot tl) = Annot(one (Tstruct(tl [])))
+let darray t = Annot(one (Tarray(single_of_annot t)))
+let ddict tk tv = Annot(one (Tdict(basic_of_annot tk, single_of_annot tv)))
+let dvariant = Annot(one Tvariant)
+let dpair (Annot a) (Annot b) = Annot(fun l -> a (b l))
+let (++) = dpair
+let dnil = Annot(fun l -> l)
--- obus/oBus_types.mli
+++ obus/oBus_types.mli
@@ -49,3 +49,114 @@ val signature_of_string : string -> signature
 val string_of_basic : basic -> string
 val string_of_single : single -> string
 val string_of_sequence : sequence -> string
+
+(** {6 Annotations} *)
+
+(** The following types are used as annotations of reader/writer
+    monads and type combinators to ensure at compile-time that the
+    structure of values manipulated respect the DBus format.
+
+    For example the following constructions will be rejected because
+    they are incorrect:
+
+    [ob_dict (ob_array ob_int32) ob_string] (because the key type of a
+    dictionary must be a basic type)
+
+    [ob_array (ob_pair ob_int32 ob_string)] (because type of element
+    of an array must consist of one single type)
+
+    Moreover, in case of reader/writer monads, annotations are used to
+    ensure that the signature provided with the monad is really the
+    type of value read/written by the monad.
+*)
+
+type (+'a, +'b) annot = private Annot of (sequence -> sequence)
+  (** ['b] contains the structure of the type and ['a] is the tail
+      variable, which is used to have canonical representation of
+      sequences.
+
+      For example the sequence [tint32, tstring, tbyte tarray] is
+      encoded like that:
+
+      [('a, tint32 * (tstring * (tbyte tarray * 'a))) annot] *)
+
+type (+'a, +'b) one = ('a, 'b * 'a) annot
+    (** Annotation for one single type *)
+
+(** {8 Basic types} *)
+
+type dbasic_byte
+type dbasic_boolean
+type dbasic_int16
+type dbasic_int32
+type dbasic_int64
+type dbasic_uint16
+type dbasic_uint32
+type dbasic_uint64
+type dbasic_double
+type dbasic_string
+type dbasic_signature
+type dbasic_object_path
+
+(** {8 Basic types seen as single types} *)
+
+type 'a dbasic
+
+type dbyte = dbasic_byte dbasic
+type dboolean = dbasic_boolean dbasic
+type dint16 = dbasic_int16 dbasic
+type dint32 = dbasic_int32 dbasic
+type dint64 = dbasic_int64 dbasic
+type duint16 = dbasic_uint16 dbasic
+type duint32 = dbasic_uint32 dbasic
+type duint64 = dbasic_uint64 dbasic
+type ddouble = dbasic_double dbasic
+type dstring = dbasic_string dbasic
+type dsignature = dbasic_signature dbasic
+type dobject_path = dbasic_object_path dbasic
+
+(** {8 Container types} *)
+
+type 'a dstruct
+type 'a darray
+type ('a, 'b) ddict
+constraint 'a = _ dbasic
+type dvariant
+
+(** {8 Annotations -> types} *)
+
+type 'a basic_p = (unit, 'a) one
+constraint 'a = _ dbasic
+    (** Match annotations of one basic type *)
+type 'a single_p = (unit, 'a) one
+    (** Match annotations of one single type *)
+type 'a sequence_p = (unit, 'a) annot
+    (** Match any annotations *)
+
+val basic_of_annot : 'a basic_p -> basic
+val single_of_annot : 'a single_p -> single
+val sequence_of_annot : 'a sequence_p -> sequence
+
+(** {8 Typed constructors} *)
+
+val dbyte : (_, dbyte) one
+val dboolean : (_, dboolean) one
+val dint16 : (_, dint16) one
+val dint32 : (_, dint32) one
+val dint64 : (_, dint64) one
+val duint16 : (_, duint16) one
+val duint32 : (_, duint32) one
+val duint64 : (_, duint64) one
+val ddouble : (_, ddouble) one
+val dstring : (_, dstring) one
+val dsignature : (_, dsignature) one
+val dobject_path : (_, dobject_path) one
+
+val dstruct : 'a sequence_p -> (_, 'a dstruct) one
+val darray : 'a single_p -> (_, 'a darray) one
+val ddict : 'a basic_p -> 'b single_p -> (_, ('a, 'b) ddict) one
+val dvariant : (_, dvariant) one
+
+val dpair : ('a, 'b) annot -> ('c, 'a) annot -> ('c, 'b) annot
+val (++) : ('a, 'b) annot -> ('c, 'a) annot -> ('c, 'b) annot
+val dnil : ('a, 'a) annot
