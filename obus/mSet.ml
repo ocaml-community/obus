28fd8982e82cd36074aa5695b15fa68c90e19812
Jeremie Dimino <jeremie@dimino.org>
2008-10-31 23:17:56 +0100

    [obus/mset] allow to deal with nodes, and separate nodes from their sets
    darcs-hash:20081031221756-c41ad-96f5a109820947f247ba129f76e11d2573caa9b9

--- obus/mSet.ml
+++ obus/mSet.ml
@@ -8,28 +8,24 @@
  *)
 
 type 'a prev =
-  | Set of 'a set
+  | Set of 'a t
   | Node of 'a node
   | Alone
 
 and 'a node = {
-  set : 'a set;
   mutable prev : 'a prev;
   mutable next : 'a node option;
   data : 'a;
 }
 
-and 'a set = 'a node option ref
-
-type 'a t = 'a set
+and 'a t = 'a node option ref
 
 let make _ = ref None
 let is_empty s = !s = None
 
 let add s x =
   let first = !s in
-  let n = { set = s;
-            prev = Set s;
+  let n = { prev = Set s;
             next = first;
             data = x } in
   begin match first with
@@ -39,31 +35,36 @@ let add s x =
   s := Some n;
   n
 
-let enabled n = n.prev = Alone
+let is_alone n = n.prev = Alone
 
-let disable n =
+let remove n =
   begin match n.next with
     | None -> ()
-    | Some n -> n.prev <- n.prev
+    | Some next -> next.prev <- n.prev
   end;
   begin match n.prev with
     | Set s -> s := n.next
-    | Node p -> p.next <- n.next
+    | Node prev -> prev.next <- n.next
     | Alone -> ()
   end;
   n.prev <- Alone;
   n.next <- None
 
-let enable n =
-  if n.prev = Alone then begin
-    let first = !(n.set) in
-    n.next <- first;
-    n.prev <- Set n.set;
-    n.set := Some n;
-    match first with
-      | Some f -> f.prev <- Node n
-      | None -> ()
-  end
+let node x = {
+  prev = Alone;
+  next = None;
+  data = x;
+}
+
+let insert n set =
+  remove n;
+  let first = !set in
+  n.next <- first;
+  n.prev <- Set set;
+  set := Some n;
+  match first with
+    | Some f -> f.prev <- Node n
+    | None -> ()
 
 let iter f s =
   let rec aux = function
--- obus/mSet.mli
+++ obus/mSet.mli
@@ -9,28 +9,15 @@
 
 (** Mutable set of non-comparable objects *)
 
-type 'a t
+(** {6 Sets} *)
 
-type 'a node
-  (** A single element *)
+type 'a t
+  (** A set of node *)
 
 val make : unit -> 'a t
   (** Create a new empty set *)
 
 val is_empty : 'a t -> bool
-val add : 'a t -> 'a -> 'a node
-
-val enabled : 'a node -> bool
-  (** Tell weather a node is in the set it was originally added *)
-
-val enable : 'a node -> unit
-  (** Ensure the node is in its set *)
-
-val disable : 'a node -> unit
-  (** Remove the node from its set.
-
-      Note: It is immediatly disabled, i.e. if we are iterating on the
-      set, and it has not yet been reached, it will be skipped *)
 
 val iter : ('a -> unit) -> 'a t -> unit
   (** Iteration over elements of the set. It is OK to modify the set
@@ -44,4 +31,26 @@ val filter : ('a -> 'a option) t -> 'a -> 'a option
       return [None] *)
 
 val clear : 'a t -> unit
-  (** Disable all elements *)
+  (** Remove all elements *)
+
+(** {6 Nodes} *)
+
+type 'a node
+  (** A single element *)
+
+val node : 'a -> 'a node
+  (** Create a new alone node *)
+
+val add : 'a t -> 'a -> 'a node
+  (** Create a new node and add it immedialty to the given set *)
+
+val insert : 'a node -> 'a t -> unit
+  (** Insert a node at the begining of a set. If the node is already
+      in a set then it is first removed then inserted in the new
+      set. *)
+
+val remove : 'a node -> unit
+  (** Remove an element from its set. Do nothing if it is alone *)
+
+val is_alone : 'a node -> bool
+  (** Return weather a node is alone or in a set *)
