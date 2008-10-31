cd44a7c5b3ef6cd3589d609c07be641a567f7fb7
Jeremie Dimino <jeremie@dimino.org>
2008-10-31 23:17:17 +0100

    [obus/mqueue] simplify message queues
    darcs-hash:20081031221717-c41ad-5e1bc7c675653cd2f915a97703db7260d9fd3293

--- obus/mQueue.mli
+++ obus/mQueue.mli
@@ -9,29 +9,12 @@
 
 (** Message queue *)
 
-type 'a t
+type 'a t = {
+  queued : 'a Queue.t;
+  waiters : 'a Lwt.t Queue.t;
+}
 
 val create : unit -> 'a t
 
 val put : 'a -> 'a t -> unit
-
 val get : 'a t -> 'a Lwt.t
-  (** Never fail, return a waiting thread if the queue is empty *)
-
-val clear : 'a t -> unit
-
-val abort : 'a t -> exn -> unit
-  (** Abort the queue:
-
-      - clear the queue
-      - all future operation will fail with this exception
-      - wakeup all waiters with the given exception *)
-
-val aborted : 'a t -> exn option
-
-(** Queue can be up or down, down means that {!get} will always return
-    a waiting thread *)
-
-val is_up : 'a t -> bool
-val set_up : 'a t -> unit
-val set_down : 'a t -> unit
