3a46f3de020fc3517e5f7911c55c5b99c08f4332
Jeremie Dimino <jeremie@dimino.org>
2009-09-20 10:20:00 +0200

    put transport stuff into a separate module
    Ignore-this: 9ed0fa7ae6c5b414f57ac6e0fa400938
    
    darcs-hash:20090920082000-c41ad-1ecdc16109f5050b36828f20c6acc9c2d28e1fae

---
+++ src/oBus_transport.ml
@@ -0,0 +1,128 @@
+(*
+ * oBus_transport.ml
+ * -----------------
+ * Copyright : (c) 2009, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, an ocaml implemtation of dbus.
+ *)
+
+
+open Unix
+open Lwt
+open OBus_address
+
+module Log = OBus_private_log.Make(struct let section = "transport" end)
+
+type transport = {
+  recv : unit -> OBus_message.t Lwt.t;
+  send : OBus_message.t -> unit Lwt.t;
+  shutdown : unit -> unit Lwt.t;
+}
+
+let make_transport ~recv ~send ~shutdown = { recv = recv; send = send; shutdown = shutdown }
+
+let recv { recv = recv } = recv ()
+let send { send = send } message = send message
+let shutdown { shutdown = shutdown } = shutdown ()
+
+let socket fd ic oc =
+  { recv = (fun _ -> OBus_wire.read_message ic);
+    send = (fun msg -> OBus_wire.write_message oc msg);
+    shutdown = (fun _ ->
+                  lwt () = Lwt_io.close ic <&> Lwt_io.close oc in
+                  Lwt_unix.shutdown fd SHUTDOWN_ALL;
+                  Lwt_unix.close fd;
+                  return ()) }
+
+let loopback _ =
+  let mvar = Lwt_mvar.create () in
+  { recv = (fun _ -> Lwt_mvar.take mvar);
+    send = (fun m -> Lwt_mvar.put m mvar);
+    shutdown = return }
+
+let make_socket domain typ addr =
+  let fd = Lwt_unix.socket domain typ 0 in
+  try_lwt
+    lwt () = Lwt_unix.connect fd addr in
+    return fd
+  with exn ->
+    Lwt_unix.close fd;
+    fail exn
+
+let transport_of_addresses ?mechanisms addresses =
+  let rec try_one domain typ addr fallback x =
+    try_lwt
+      lwt fd = make_socket domain typ addr in
+      let ic = Lwt_io.make ~mode:Lwt_io.input (Lwt_unix.read fd)
+      and oc = Lwt_io.make ~mode:Lwt_io.output (Lwt_unix.write fd) in
+      lwt guid = OBus_auth.client_authenticate ?mechanisms (auth_stream_of_channels chans) in
+      return (guid, socket fd ic oc)
+    with exn ->
+      OBus_log.log "transport creation failed for address: domain=%s typ=%s addr=%s: %s"
+        (match domain with
+           | PF_UNIX -> "unix"
+           | PF_INET -> "inet"
+           | PF_INET6 -> "inet6")
+        (match typ with
+           | SOCK_STREAM -> "stream"
+           | SOCK_DGRAM -> "dgram"
+           | SOCK_RAW -> "raw"
+           | SOCK_SEQPACKET -> "seqpacket")
+        (match addr with
+           | ADDR_UNIX path -> sprintf "unix(%s)" path
+           | ADDR_INET(addr, port) -> sprintf "inet(%s,%d)" (string_of_inet_addr addr) port)
+        (Util.string_of_exn exn);
+      fallback x
+  in
+  let rec aux = function
+    | [] -> failwith "no working DBus address found"
+    | (desc, _) :: rest ->
+        match desc with
+          | Unix_path path ->
+              try_one PF_UNIX SOCK_STREAM (ADDR_UNIX(path))
+                aux rest
+
+          | Unix_abstract path ->
+              try_one PF_UNIX SOCK_STREAM (ADDR_UNIX("\x00" ^ path))
+                aux rest
+
+          | Unix_tmpdir _ ->
+              OBus_log.error "unix tmpdir can only be used as a listening address";
+              aux rest
+
+          | Tcp { tcp_host = host; tcp_port = port; tcp_family = family } ->
+              let opts = [AI_SOCKTYPE SOCK_STREAM] in
+              let opts = match family with
+                | Some `Ipv4 -> AI_FAMILY PF_INET :: opts
+                | Some `Ipv6 -> AI_FAMILY PF_INET6 :: opts
+                | None -> opts in
+              let rec try_all = function
+                | [] -> aux rest
+                | ai :: ais -> try_one ai.ai_family ai.ai_socktype ai.ai_addr try_all ais
+              in
+              try_all (getaddrinfo host port opts)
+
+          | Autolaunch ->
+              lwt addresses =
+                try_lwt
+                  lwt uuid = Lazy.force OBus_info.machine_uuid in
+                  lwt line =
+                    try_lwt
+                      Lwt_process.pread_line ("dbus-launch",
+                                              [|"dbus-launch"; "--autolaunch"; OBus_uuid.to_string uuid; "--binary-syntax"|])
+                    with exn ->
+                      OBus_log.log "autolaunch failed: %s" (Util.string_of_exn exn);
+                      fail exn
+                  in
+                  let line = try String.sub line 0 (String.index line '\000') with _ -> line in
+                  return (OBus_address.of_string line)
+                with exn ->
+                  return []
+              in
+              aux (addresses @ rest)
+
+          | _ ->
+              aux rest
+  in
+  aux addresses
---
+++ src/oBus_transport.mli
@@ -0,0 +1,45 @@
+(*
+ * oBus_transport.mli
+ * ------------------
+ * Copyright : (c) 2009, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of obus, an ocaml implemtation of dbus.
+ *)
+
+(** Low-level transporting of messages *)
+
+type t
+  (** Type of message transport *)
+
+val recv : t -> OBus_message.t Lwt.t
+  (** [recv tr] receives one message from the given transport *)
+
+val send : t -> OBus_message.t -> unit Lwt.t
+  (** [send tr msg] sends [msg] over the transport [tr]. *)
+
+val shutdown : t -> unit Lwt.t
+  (** [shutdown tr] free resources allocated by the given transport *)
+
+val make :
+  recv : unit -> OBus_message.t Lwt.t ->
+  send : OBus_message.t -> unit Lwt.t ->
+  shutdown : unit -> unit Lwt.t
+  (** [make ~recv ~send ~shutdown] creates a new transport from the
+      given functions.
+
+      Notes:
+      - message reading/writing are serialized by obus, so there is no
+      need to handle concurrent access to transport
+  *)
+
+val loopback : unit -> t
+  (** Loopback transport, each message sent is received on the same
+      transport *)
+
+val transport_of_addresses :
+  ?mechanisms : OBus_auth.client_mechanism list ->
+  OBus_address.t list ->
+  (OBus_address.guid * t) Lwt.t
+    (** Try to make a working transport from a list of
+        addresses. Return also the guid of the server address *)
