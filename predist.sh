84f7f5eb06114b8d21491d98dfbef126079a44ac
Jeremie Dimino <jeremie@dimino.org>
2010-09-07 10:51:01 +0200

    changes in the manual
    Ignore-this: 864bbd2200e65db009b30c8544db1895
    
    darcs-hash:20100907085101-c41ad-892964095d370094d08178b0eb3827db53cded84

--- predist.sh
+++ predist.sh
@@ -4,7 +4,7 @@
 [ -d "$DARCS_REPO" ] && darcs changes --repodir "$DARCS_REPO" > CHANGES.darcs
 
 # Build the user manual for release
-make -C manual manual.pdf manual-colored.pdf clean-melt
+make -C manual manual.pdf clean-melt
 
 # The makefile contains no useful rules for the end user
 rm -f Makefile
