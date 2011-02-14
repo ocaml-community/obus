83e0931ddcdde1c9f5ad44b15c8c700f001baf55
Jeremie Dimino <jeremie@dimino.org>
2011-02-14 16:47:50 +0100

    use listings in the manual
    Ignore-this: ea394a5fd82a557609eb106d1d5fcaf
    
    darcs-hash:20110214154750-c41ad-5a660a7b78b5b04e30aee7aba4df642cb60e7581

--- predist.sh
+++ predist.sh
@@ -4,7 +4,7 @@
 [ -d "$DARCS_REPO" ] && darcs changes --repodir "$DARCS_REPO" > CHANGES.darcs
 
 # Build the user manual for release
-make -C manual manual.pdf clean-melt
+make -C manual all clean-aux
 
 # Add OASIS stuff
 oasis setup
