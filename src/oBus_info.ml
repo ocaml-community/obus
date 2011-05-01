cd47d8b0527d1bbe170491091863cf56e3354aea
Jeremie Dimino <jeremie@dimino.org>
2011-05-01 21:44:33 +0200

    read /etc/machine-id if the machine uuid file cannot be read
    Ignore-this: bf2258b8b4b28c00dcd82471efcefc35
    
    darcs-hash:20110501194433-c41ad-27ff1f812b3ee6a826fd4f98d1ccf5383a3e3226

--- src/oBus_info.ml
+++ src/oBus_info.ml
@@ -17,14 +17,20 @@ let protocol_version = 1
 let max_name_length = OBus_protocol.max_name_length
 let max_message_size = OBus_protocol.max_message_size
 
-let machine_uuid = lazy(
+let read_uuid_file file =
   try_lwt
-    lwt line = Lwt_io.with_file ~mode:Lwt_io.input OBus_config.machine_uuid_file Lwt_io.read_line in
+    lwt line = Lwt_io.with_file ~mode:Lwt_io.input file Lwt_io.read_line in
     return (OBus_uuid.of_string line)
   with exn ->
-    lwt () =
-      Lwt_log.error_f ~section "failed to read the local machine uuid from file %S: %s"
-        OBus_config.machine_uuid_file (Printexc.to_string exn)
-    in
+    ignore (Lwt_log.error_f ~section ~exn "failed to read the local machine uuid from file %S" file);
     raise_lwt exn
+
+let machine_uuid = lazy(
+  try_lwt
+    read_uuid_file OBus_config.machine_uuid_file
+  with exn ->
+    try_lwt
+      read_uuid_file "/etc/machine-id"
+    with _ ->
+      raise_lwt exn
 )
