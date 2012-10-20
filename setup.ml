2b1d1904d704e4c960ba3c19efacc8e4920a8039
Jérémie Dimino <jeremie@dimino.org>
2012-10-20 12:17:14 +0200

    Add a Makefile
    It compiles setup.ml to a native executable.

--- setup.ml
+++ setup.ml
@@ -3,12 +3,9 @@
  * --------
  * Copyright : (c) 2011, Jeremie Dimino <jeremie@dimino.org>
  * Licence   : BSD3
- *
- * This file is a part of obus, an ocaml implementation of D-Bus.
  *)
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 7f47a529f70709161149c201ccd90f0b) *)
 #use "topfind";;
 #require "oasis.dynrun";;
 open OASISDynRun;;
